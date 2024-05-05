8f51936390fac6e6e115fec00d1191a07d66f094
assilbekov <askhat.assilbekov@gmail.com>
2024-05-05 14:14:47 -0300

    add scanning flow

--- main.go
+++ main.go
@@ -1,5 +1,22 @@
 package main
 
+import "bridge/pkg/types"
+
+var (
+	hpScanner    = &types.ScannerHP{}
+	epsonScanner = &types.ScannerEpson{}
+	linuxPC      = &types.LinuxPC{}
+	macPC        = &types.MacPC{}
+	windowsPC    = &types.WindowsPC{}
+)
+
 func main() {
-	//
+	linuxPC.AddScanner(hpScanner)
+	linuxPC.Scan()
+
+	macPC.AddScanner(epsonScanner)
+	macPC.Scan()
+
+	windowsPC.AddScanner(hpScanner)
+	windowsPC.Scan()
 }
